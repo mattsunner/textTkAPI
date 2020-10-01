47c311515077e343da5bf4f066cc7cc70c6ddeaa
Matt Sunner <sunner.matt@gmail.com>
2020-09-30 22:48:42 -0500

    Create Tokenize Method
    Write tand run initial test of the tokenize method.

--- textTkAPI/application.py
+++ textTkAPI/application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import nltk
 
 
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/tokenize', methods=['GET', 'POST'])
+def tokenize():
+    sentence = request.form['tokens']
+    tokens = nltk.word_tokenize(sentence)
+    return render_template('tokenize.html', tokens=tokens)
 
 
 if __name__ == '__main__':
